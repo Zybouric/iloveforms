7150df0342fb192ff588c7809f201f4eeeb9ce6b
zybouric <hoang_christine@yahoo.fr>
2018-10-31 02:19:48 +0100

    maj formulaire + un peu de css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,18 +6,25 @@ class UsersController < ApplicationController
 
 
    def create
-     @user = User.new('username' => params[:user][:username], 'email' => params[:user][:email], 'bio' => params[:user][:bio])
-     if @user.save
-      redirect_to users_path
-  	else
-      render 'new'
-  	end
+   		if params.nil?
+     		@user = User.new('username' => params[:name], 'email' => params[:email], 'bio' => params[:bio])
+	    else
+     		@user = User.new('username' => params[:user][:username], 'email' => params[:user][:email], 'bio' => params[:user][:bio])
+     	end
+	    if @user.save
+	      redirect_to users_path
+	  	else
+	      render 'new'
+	  	end
    end 
+
   
   def index
      @users = User.all
   end
+
   def show
-  	@users = User.all
+  	 @users = User.all
   end
+
 end
